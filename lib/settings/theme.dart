13a9241b64ef98b829c05646efd18ebdc22e1d65
aamian <aamian@mrmilu.com>
2023-04-09 11:22:22 +0200

    Add settings folder

--- lib/settings/theme.dart
+++ lib/settings/theme.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
 abstract class AppTheme {
-  static final Color focusNavBarItemColor =
-      Colors.amber[800]!; // coverage:ignore-line
+  static final Color focusNavBarItemColor = Colors.amber[800]!;
 }
