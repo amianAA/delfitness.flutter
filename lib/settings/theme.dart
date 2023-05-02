190798e9348c3db0d0ed054d4edeed0f341c5eb4
aamian <aamian@mrmilu.com>
2023-05-03 00:37:10 +0200

    Feature/testing setup

--- lib/settings/theme.dart
+++ lib/settings/theme.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
 abstract class AppTheme {
-  static final Color focusNavBarItemColor = Colors.amber[800]!;
+  static final Color focusNavBarItemColor =
+      Colors.amber[800]!; // coverage:ignore-line
 }
